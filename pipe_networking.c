0a8db85bf7c4d61566aa8ee195db4da0673df4d1
Stanley Huang <shuang50@stuy.edu>
2024-12-19 14:50:38 -0500

    end of class

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,12 +10,16 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_setup() {
-  int from_client = 0;
+  char * known = "./known";
+  mkfifo(known, 0666);
+  int from_client;
+  from_client = open(known, O_RDONLY, 0666);
+  remove(known);
   return from_client;
 }
 
 /*=========================
-  server_handshake 
+  server_handshake
   args: int * to_client
 
   Performs the server side pipe 3 way handshake.
@@ -24,6 +28,21 @@ int server_setup() {
   returns the file descriptor for the upstream pipe (see server setup).
   =========================*/
 int server_handshake(int *to_client) {
+  server_setup();
+  int synack;
+  char * known = "./known";
+  char * private = "./private";
+  synack = open(known, O_RDONLY, 0666);
+  char buffer[256];
+  read(synack, buffer, sizeof(buffer));
+  close(synack);
+  int random = atoi(buffer);
+  random++
+  char* ack;
+  sprintf(ack, "%d", random);
+  int joe;
+  joe = open(private, O_WRONLY, 0666);
+  write(joe, ack, sizeof(ack));
   int from_client;
   return from_client;
 }
@@ -39,7 +58,25 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
+  int syn;
+  char * known = "./known";
+  char * pid;
+  sprintf(pid, getpid());
+  syn = open(known, O_WRONLY, 0666);
+  to_server = &syn;
+  int w = write(syn, pid, strlen(pid));
+  remove(known);
   int from_server;
+  char * private = "./private";
+  mkfifo(private, 0666);
+  char buffer[256];
+  int random;
+  int * num;
+  random = open("/dev/urandom", num, sizeof(num));
+  sprintf(buffer, "%d", num)
+  from_server = read(private, buffer, sizeof(buffer));
+  remove(private);
+  close(random);
   return from_server;
 }
 
@@ -56,5 +93,3 @@ int server_connect(int from_client) {
   int to_client  = 0;
   return to_client;
 }
-
-
